344d5bbbfadfc124ec058a53822a10fe5c1f91fa
saxenasaksham46 <saxenasaksham46>
2025-06-23 10:53:27 +0530

    fix:Change baseurl in frontend

--- wubble-quicktune-frontend/src/helpers/axios.js
+++ wubble-quicktune-frontend/src/helpers/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://wubble-quicktune-ai.onrender.com//api'
+const BASE_URL = 'https://wubble-quicktune-ai.onrender.com/api'
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
